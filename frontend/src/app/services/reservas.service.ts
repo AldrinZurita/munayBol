ac050c1205ac1286972e008124029d9420322ae0
Arturo10021 <arturo.guzman@ucb.edu.bo>
2025-09-20 15:50:12 -0400

    consumir datos del back

---
+++ frontend/src/app/services/reservas.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Reserva {
+  id_reserva: number;
+  fecha_reserva: string;
+  fecha_caducidad: string;
+  num_habitacion: string;
+  codigo_hotel: number;
+  fecha_creacion: string;
+  ci_usuario: number;
+  id_pago: number;
+}
+
+@Injectable({ providedIn: 'root' })
+export class ReservasService {
+  private apiUrl = '/api/reservas/';
+
+  constructor(private http: HttpClient) {}
+
+  getReservas(): Observable<Reserva[]> {
+    return this.http.get<Reserva[]>(this.apiUrl);
+  }
+}
