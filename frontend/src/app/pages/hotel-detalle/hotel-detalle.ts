a4efe84bc0e9d160c46cc21e068fe1f5e7b216bb
Camila <camila.carvajal@ucb.edu.bo>
2025-10-14 11:44:14 -0400

    feat: mostrar habitaciones dentro de detalle hotel

--- frontend/src/app/pages/hotel-detalle/hotel-detalle.ts
+++ frontend/src/app/pages/hotel-detalle/hotel-detalle.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { Hotel } from '../../interfaces/hotel.interface';
 import { HotelService } from '../../services/hotel.service';
+import { HabitacionService } from '../../services/habitacion.service';
+import { Habitacion } from '../../interfaces/habitacion.interface';
 
 @Component({
   selector: 'app-hotel-detalle',
@@ -13,13 +15,16 @@ import { HotelService } from '../../services/hotel.service';
 })
 export class HotelDetalleComponent implements OnInit {
   hotel: Hotel | null = null;
+  habitaciones: Habitacion[] = [];
   cargando = false;
+  cargandoHabitaciones = false;
   error = '';
 
   constructor(
     private route: ActivatedRoute,
-    private hotelService: HotelService
-  ) {}
+    private hotelService: HotelService,
+    private habitacionService: HabitacionService,
+  ) { }
 
   ngOnInit(): void {
     this.cargando = true;
@@ -33,11 +38,39 @@ export class HotelDetalleComponent implements OnInit {
       next: (hotel) => {
         this.hotel = hotel;
         this.cargando = false;
+        this.getHabitaciones();
       },
       error: (e) => {
         this.error = 'No se pudo cargar el hotel.';
         this.cargando = false;
       }
     });
+    
+    
+
   }
+
+
+  
+  getHabitaciones(): void {
+    if (!this.hotel || !this.hotel.id_hotel) {
+      this.error = 'No se ha seleccionado un hotel válido';
+      return;
+    }
+
+    this.cargandoHabitaciones = true;
+    this.habitacionService.getHabitacionesPorHotel(this.hotel.id_hotel).subscribe({
+    next: (habitaciones: Habitacion[]) => {
+      this.habitaciones = habitaciones;
+      this.cargandoHabitaciones = false;
+    },
+    error: (err) => {
+      console.error('Error al obtener habitaciones:', err);
+      this.error = 'No se pudo cargar la lista de habitaciones';
+      this.cargandoHabitaciones = false;
+    }
+  });
+
+  }
+
 }
