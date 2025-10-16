209e19b1d0d58eb720e30ab99bfd712115cbc34f
Juan Pablo <jpriveram01@gmail.com>
2025-10-15 22:03:48 -0400

    feat: seagrego el boton para ver el detalle de la reserva

--- frontend/src/app/pages/perfil/perfil.ts
+++ frontend/src/app/pages/perfil/perfil.ts
@@ -2,14 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../services/auth.service';
 import { Usuario } from '../../interfaces/usuario.interface';
-import { ReservasService } from '../../services/reservas.service'; // 1. Import ReservasService
-import { Reserva } from '../../interfaces/reserva.interface';     // 2. Import Reserva interface
-import { RouterModule } from '@angular/router';
+import { ReservasService } from '../../services/reservas.service';
+import { Reserva } from '../../interfaces/reserva.interface';
+import { RouterModule } from '@angular/router'; // 1. Importar RouterModule
 
 @Component({
   selector: 'app-perfil',
   standalone: true,
-  imports: [CommonModule, RouterModule], // Add RouterModule for the "Explorar Paquetes" button
+  imports: [CommonModule, RouterModule], // 2. Añadir RouterModule a las importaciones
   templateUrl: './perfil.html',
   styleUrls: ['./perfil.scss']
 })
@@ -17,21 +17,19 @@ export class Perfil implements OnInit {
   usuario: Usuario | null = null;
   cargando = true;
 
-  // --- NEW PROPERTIES for reservations ---
   reservas: Reserva[] = [];
   cargandoReservas = true;
   errorReservas = '';
 
   constructor(
     private authService: AuthService,
-    private reservasService: ReservasService // 3. Inject ReservasService
+    private reservasService: ReservasService
   ) {}
 
   ngOnInit(): void {
     this.usuario = this.authService.getUser();
     this.cargando = false;
 
-    // --- NEW LOGIC to fetch reservations ---
     if (this.usuario) {
       this.reservasService.getMisReservas().subscribe({
         next: (data) => {
@@ -44,9 +42,7 @@ export class Perfil implements OnInit {
         }
       });
     } else {
-      // If there's no user, we can't fetch reservations.
-      this.cargandoReservas = false; 
-      this.errorReservas = 'Debes iniciar sesión para ver tus reservas.';
+      this.cargandoReservas = false;
     }
   }
 }
