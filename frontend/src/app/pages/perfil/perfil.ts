bc6180e8fe966475e46a110bebc83cd210e00737
Juan Pablo <jpriveram01@gmail.com>
2025-10-15 21:57:01 -0400

    feat: se muestran las reservas en el perfil del usario

--- frontend/src/app/pages/perfil/perfil.ts
+++ frontend/src/app/pages/perfil/perfil.ts
@@ -2,11 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../services/auth.service';
 import { Usuario } from '../../interfaces/usuario.interface';
+import { ReservasService } from '../../services/reservas.service'; // 1. Import ReservasService
+import { Reserva } from '../../interfaces/reserva.interface';     // 2. Import Reserva interface
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-perfil',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule], // Add RouterModule for the "Explorar Paquetes" button
   templateUrl: './perfil.html',
   styleUrls: ['./perfil.scss']
 })
@@ -14,11 +17,37 @@ export class Perfil implements OnInit {
   usuario: Usuario | null = null;
   cargando = true;
 
-  constructor(private authService: AuthService) {}
+  // --- NEW PROPERTIES for reservations ---
+  reservas: Reserva[] = [];
+  cargandoReservas = true;
+  errorReservas = '';
+
+  constructor(
+    private authService: AuthService,
+    private reservasService: ReservasService // 3. Inject ReservasService
+  ) {}
 
   ngOnInit(): void {
-    // Retrieve the user data from the authentication service
     this.usuario = this.authService.getUser();
     this.cargando = false;
+
+    // --- NEW LOGIC to fetch reservations ---
+    if (this.usuario) {
+      this.reservasService.getMisReservas().subscribe({
+        next: (data) => {
+          this.reservas = data;
+          this.cargandoReservas = false;
+        },
+        error: () => {
+          this.errorReservas = 'No se pudieron cargar tus reservas.';
+          this.cargandoReservas = false;
+        }
+      });
+    } else {
+      // If there's no user, we can't fetch reservations.
+      this.cargandoReservas = false; 
+      this.errorReservas = 'Debes iniciar sesión para ver tus reservas.';
+    }
   }
 }
+
