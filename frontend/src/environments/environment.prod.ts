3b4e039d9e877d0236b82a0be7691d95a6abba7c
AldrinZurita <axzt2004@gmail.com>
2025-12-02 11:58:50 -0400

    fix: api url correction

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   // URL de tu Backend en Render (sin barra al final)
-  apiUrl: 'https://munaybol-backend.onrender.com', 
+  apiUrl: 'https://munaybol-backend.onrender.com/api/', 
   
   googleClientId: '643155360761-mvfq6qbbl1qbh6fthl933q74cgijb0pl.apps.googleusercontent.com',
   
